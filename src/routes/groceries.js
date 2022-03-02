a8a847891e5d9ad5d2e26aebefe60f76f5921cd9
Kristian Nissen <kristian.nissen@gmail.com>
2022-03-02 12:03:23 +0100

    Working on #4 Fixes #15

--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -59,30 +59,36 @@ const Groceries = () => {
     });
   };
 
-  const handleDelete = (e) => {
-    setGroceries(
-      groceries.filter((g) => e.target.parentElement.parentElement.dataset.key !== g.id)
-    );
+  const handleDelete = (id) => {
+    setGroceries(groceries.filter((g) => id !== g.id));
   };
 
   return (
     <div className="min-w-full">
+      <h1>Grocery List</h1>
       <div className="min-w-full">
         {groceries.map((grocery) => (
-          <div className="flex flex-row h-12" key={grocery.id} data-key={grocery.id}>
+          <div
+            className="flex flex-row h-12"
+            key={grocery.id}
+            data-key={grocery.id}
+          >
             <div className="basis-3/4 px-1.5 py-1.5">{grocery.name}</div>
-            <div className="basis-1/4 px-1.5 py-1.5 justify-end">
-      <span className="absolute top-0 bottom-0 right-0 px-4 py-3" onClick={handleDelete}>
-        <svg
-          className="fill-current h-6 w-6 text-red-500 ml-1"
-          role="button"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <title>Close</title>
-          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-        </svg>
-      </span>
+            <div className="basis-1/4 px-1.5 py-1.5 grid justify-items-end">
+              <span
+                className="top-0 bottom-0 w-6 h-6"
+                onClick={() => handleDelete(grocery.id)}
+              >
+                <svg
+                  className="fill-current text-rose-500 w-6 h-6"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </span>
             </div>
           </div>
         ))}
@@ -90,7 +96,7 @@ const Groceries = () => {
       <div className="">
         <div className="form_group">
           <label htmlFor="entry" className="field__label">
-            Entry
+            Add Grocery
           </label>
           <input
             type="text"
