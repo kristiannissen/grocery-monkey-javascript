281533a5fc02cbc4fc8cf7c1a1baa91cbbae9afa
Kristian Nissen <kristian.nissen@gmail.com>
2022-03-28 14:24:15 +0200

    Fixing [InjectManifest] InjectManifest must have required property 'globDirectory'.

---
+++ build/sw.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+self.addEventListener("install", (event) => {
+  console.log("install");
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["/groceries"]);
+    })
+  );
+});
