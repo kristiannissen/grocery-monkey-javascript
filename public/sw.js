ff789baf0feee353b0a1426bf0a13e0700743be0
Kristian Nissen <kristian.nissen@gmail.com>
2022-04-06 22:39:21 +0200

    Working on #32

--- public/sw.js
+++ public/sw.js
@@ -19,11 +19,40 @@ self.addEventListener("install", (event) => {
   );
 });
 
+const update = (request) =>
+  caches
+    .open(CACHE_KEY)
+    .then((cache) =>
+      fetch(request).then((response) => cache.put(request.url, response))
+    );
+
+const network = (request) =>
+  new Promise((resolve, reject) => {
+    fetch(request.clone()).then((response) => {
+      resolve(response);
+      // Update the cahce
+      update(request);
+    }, reject);
+  });
+
+const cached = (request) =>
+  caches
+    .open(CACHE_KEY)
+    .then((cache) => cache.match(request.url).then((matching) => matching));
+
 /**
  * Handle fetch events
  * https://gist.github.com/JMPerez/8ca8d5ffcc0cc45a8b4e1c279efd8a94
  */
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", (event) => {
+  // Store in cache
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      network(event.request).catch(() => cached(event.request))
+    );
+    event.waitUntil(update(event.request));
+  }
+});
 
 /**
  * BackgroundSync
