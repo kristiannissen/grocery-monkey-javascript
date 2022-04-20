de0cbf43d96afaa6624ec49e54ec163367632dcc
Kristian Nissen <kristian.nissen@gmail.com>
2022-04-20 11:32:31 +0200

    Fixes #40

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,34 @@
  */
 import React from "react";
 import { createPortal } from "react-dom";
+import { Link } from "react-router-dom";
+import { useShell } from "./../context/shell";
+
+import "./header.css";
 
 const Header = () => {
+  const shell = useShell();
+
   const markup = () => {
     return (
-      <header className="">
-        <h1>Hello Kitty</h1>
+      <header className="header">
+        <span className="header__title">Grocery Monkey</span>
+        <nav className="header__nav">
+          <Link to="/" className="header__nav_link">
+            Home
+          </Link>
+          <Link to="/groceries" className="header__nav_link">
+            Groceries
+          </Link>
+          <Link to="/mealplan" className="header__nav_link">
+            Mealplan
+          </Link>
+        </nav>
       </header>
     );
   };
 
-  return createPortal(markup(), document.getElementById("app__shell_header"));
+  return createPortal(markup(), document.getElementById(shell.header));
 };
 
 export default Header;
